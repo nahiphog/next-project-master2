dbc7d751ed4cd805e8bcff51a84bbd08961b55e1
jc-29 <jonathanchng703@gmail.com>
2019-11-21 17:44:57 +0800

    export and configured csrf and login manager in app.py

--- backend/app.py
+++ backend/app.py
@@ -2,11 +2,21 @@ import os
 import config
 from flask import Flask
 from models.base_model import db
+from flask_wtf.csrf import CSRFProtect
+from flask_login import LoginManager
+from models.user import User
+
+login_manager = LoginManager()
 
 web_dir = os.path.join(os.path.dirname(
     os.path.abspath(__file__)), 'instagram_web')
 
 app = Flask('NEXTAGRAM', root_path=web_dir)
+csrf = CSRFProtect(app)
+login_manager.init_app(app)
+login_manager.login_view = 'sessions.new'
+login_manager.login_message = 'Please login before proceeding...'
+login_manager.login_message_category = "warning"
 
 if os.getenv('FLASK_ENV') == 'production':
     app.config.from_object("config.ProductionConfig")
@@ -25,3 +35,7 @@ def _db_close(exc):
         print(db)
         print(db.close())
     return exc
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User_.get_or_none(id= user_id)
--- backend/instagram_api/blueprints/users/views.py
+++ backend/instagram_api/blueprints/users/views.py
@@ -1,9 +1,20 @@
 from flask import Blueprint, jsonify
+from models.user import User
 
 users_api_blueprint = Blueprint('users_api',
-                             __name__,
-                             template_folder='templates')
+                             __name__)
+
+
 
 @users_api_blueprint.route('/', methods=['GET'])
-def index():
-    return "USERS API"
+def index(): #returns a list of users from database
+    user =[ 
+        {
+        'id': user.id,
+        'name': user.name,
+        'email': user.email,
+        'profile_picture': user.profile_picture
+    } for user in User.select()
+    ]
+
+    return jsonify(user), 200
