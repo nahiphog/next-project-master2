11aa39d7e1432c6ad4d0c287f63f0c78fe81fe51
jc-29 <jonathanchng703@gmail.com>
2019-11-25 14:38:10 +0800

    resolve App.js conflict

--- frontend/next-project/src/App.js
+++ frontend/next-project/src/App.js
@@ -1,11 +1,6 @@
 /* Import package components */
 import React from "react";
 import { Grid, CssBaseline } from "@material-ui/core";
-<<<<<<< HEAD
-import { Dialog, Slide } from "@material-ui/core";
-import route from "./global";
-=======
->>>>>>> ccc96023b398acf366590f8f892636c54f224809
 
 /* Import app components */
 import TopNav from "./components/TopNav";
@@ -20,36 +15,6 @@ const ContainerStyles = {
 };
 
 export default function App() {
-<<<<<<< HEAD
-  const [tab, setTab] = useState(0);
-  const [dialogOpen, setDialogOpen] = useState(false);
-  const [routeOption, setRouteOption] = useState(-1);
-
-  const routeTo = option => {
-    console.log("option: ", option);
-    if (option === route.back) {
-      setDialogOpen(false);
-    } else {
-      setDialogOpen(true);
-      setRouteOption(option);
-    }
-  };
-
-  function renderView() {
-    switch (tab) {
-      case 0:
-        return <Content message={"Tab 0"} />;
-      case 1:
-        return <Content message={"Tab 1"} />;
-      case 2:
-        return <Content message={"Tab 2"} />;
-      default:
-        return new Error("This view does not exist");
-    }
-  }
-
-=======
->>>>>>> ccc96023b398acf366590f8f892636c54f224809
   return (
     <>
       <Grid container direction="column">
