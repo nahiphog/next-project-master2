45f139e7175ed0d2ca2b05713e9aae331f963ab9
bakudan00 <lisa_suradi@yahoo.com>
2019-11-26 10:39:08 +0800

    Side bar navigation completed

--- frontend/next-project/src/components/TopNav.js
+++ frontend/next-project/src/components/TopNav.js
@@ -5,13 +5,31 @@ import {
   Toolbar,
   Avatar,
   IconButton,
-  SwipeableDrawer
+  SwipeableDrawer,
+  Grid,
+  List, ListSubheader, ListItem, ListItemIcon, ListItemText
 } from "@material-ui/core";
 import { Menu } from "@material-ui/icons";
+import FaceIcon from '@material-ui/icons/Face';
+import PersonIcon from '@material-ui/icons/Person';
+import TelegramIcon from '@material-ui/icons/Telegram';
+import InboxIcon from '@material-ui/icons/Inbox';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import BookmarkIcon from '@material-ui/icons/Bookmark';
+import ImportContactsIcon from '@material-ui/icons/ImportContacts';
 import { route } from "../global";
 
 /* Import app components */
 import DialogPage from "../components/DialogPage";
+
+const iconColor ={
+  color: '#5CB3FF'
+}
+
+const backGround ={
+  color: '#C2DFFF'
+}
 
 export default function TopNav() {
   const [open, setOpen] = useState(false);
@@ -43,7 +61,58 @@ export default function TopNav() {
           disableSwipeToOpen={false}
           PaperProps={{ style: { minWidth: "50vw" } }}
         >
-          <h4>Menu</h4>
+          {/* <h4>Menu</h4> */}
+          <br /><br /><br />
+          <Grid container direction="column">
+          <List component="nav" aria-labelledby="nested-list-subheader"
+            subheader={
+              <ListSubheader component="div" id="nested-list-subheader" style={backGround} >
+                <ListItem button>
+                  <ListItemIcon>
+                    <AccountCircleIcon style={{ fontSize: 50, color: "#1589FF"}}/>
+                  </ListItemIcon>
+              </ListItem>
+              </ListSubheader>
+          }>
+            <br />
+              <ListItem button onClick={() => routeTo(route.profilePage)}>
+                <ListItemIcon>
+                  <PersonIcon style={iconColor} />
+                </ListItemIcon>
+                <ListItemText primary="Profile" />
+              </ListItem>
+              <ListItem button onClick={() => routeTo(route.learnListPage)}>
+                <ListItemIcon>
+                  <ImportContactsIcon style={iconColor}/>
+                </ListItemIcon>
+                <ListItemText primary="Lesson List" />
+              </ListItem>
+              <ListItem button onClick={() => routeTo(route.teachListPage)}>
+                <ListItemIcon>
+                  <FaceIcon style={iconColor}/>
+                </ListItemIcon>
+                <ListItemText primary="Teach List" />
+              </ListItem>
+              <ListItem button onClick={() => routeTo(route.bookmarkListPage)}>
+                <ListItemIcon>
+                  <BookmarkIcon style={iconColor}/>
+                </ListItemIcon>
+                <ListItemText primary="Bookmark List" />
+              </ListItem>
+              <ListItem button onClick={() => routeTo(route.inboxListPage)}>
+                <ListItemIcon>
+                  <InboxIcon style={iconColor}/>
+                </ListItemIcon>
+                <ListItemText primary="Inbox" />
+              </ListItem>
+              <ListItem button onClick={() => routeTo(route.signinPage)}>
+                <ListItemIcon>
+                  <ExitToAppIcon style={iconColor}/>
+                </ListItemIcon>
+                <ListItemText primary="Signup/Login" />
+              </ListItem>
+            </List>
+          </Grid>
         </SwipeableDrawer>
       </AppBar>
       <DialogPage
